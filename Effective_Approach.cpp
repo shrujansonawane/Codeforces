b90294e3e0150fa768fe1d4509c6869786855311
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-03-20 14:12:28 +0530

    277B

---
+++ Effective_Approach.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<map>
+using namespace std;
+ 
+int main() {
+map<int,int> ma;
+long long first=0;
+long long second=0;
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+	int y;
+	cin>>y;
+	ma[y]=i;
+}
+int q;
+cin>>q;
+for(int i=0;i<q;i++){
+	int x;
+	cin>>x;
+	first+=ma[x]+1;
+	  
+	second+=n-ma[x];
+	 
+}
+ 
+cout<<first<<" "<<second;
+ 
+ 
+	return 0;
+}
