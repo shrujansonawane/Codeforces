d7bc6df8d0ccf487f0ce8fb2e02ed4dce171162b
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-05-04 19:47:16 +0530

    445A

---
+++ DZY_Loves_Chessboard.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main()
+{
+ 
+    int n, m;
+    char mp[110][110];
+    cin>>n>>m;
+    for(int i=0;i<n;++i)
+    {
+        for(int j=0;j<m;++j)
+        {
+            cin>>mp[j][i];
+            if(mp[j][i]=='.')
+            {
+                if((j+i)%2==0)
+                {mp[j][i]='B';
+              }
+                else {
+                  mp[j][i]='W';
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;++i)
+    {
+        for(int j=0;j<m;++j)
+            cout<<mp[j][i];
+        cout<<endl;
+    }
+    return 0;
+}
