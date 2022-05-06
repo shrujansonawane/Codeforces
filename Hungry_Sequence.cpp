dd7738989dbf34d25b809e2d90e0a506b5ba42bb
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-05-06 17:04:31 +0530

    327B

---
+++ Hungry_Sequence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+	long long int n,i,count(0);
+	cin>>n;
+	for(i=1;count<n;i++){
+		
+		cout<<n+i<<" ";
+		count++;
+		
+	}
+     return 0;
+}
