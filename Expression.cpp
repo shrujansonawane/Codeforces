149695b6de7d406c65440113ec8273add4b77600
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-06-30 11:42:32 +0530

    479A

---
+++ Expression.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main ()
+{
+	
+	int a,b,c;
+	cin>>a>>b>>c;
+	int ans(0);
+	
+	ans=a+b+c;
+	
+	ans=max(ans,(a+b)*c);
+	ans=max(ans,a*(b+c));
+	ans=max(ans,a*b*c);
+	
+	cout<<ans<<endl;
+	
+  	
+    return 0;
+    
+}
